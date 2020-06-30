6aa04e29fa93d1ef04ccd9ffcf01dc53b7abf2f0
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-06-30 14:24:58 -0300

    Add excluded paths to JWT verification

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,11 +1,23 @@
 const { verifyJwt } = require('../helpers/jwt')
 
 const checkJwt = (req, res, next) => {
+  // Get the 'url' from req variable and set an alias 'path'
+  const { url: path } = req
+
+  // Paths to skip JWT verification
+  const excludedPaths = ['/auth/sign-in', '/auth/sign-up']
+
+  // Check if path is excluded and transform to boolean (!!)
+  const isExcluded = !!excludedPaths.find(p => p.startsWith(path))
+
+  if (isExcluded)
+    return next()
+
   let token = req.headers['authorization']
 
   // Remove 'Bearer ' from token string
   token = token ? token.slice(7, token.length) : null
-console.log(token)
+
   if (!token)
     return res.jsonUnauthorized(null, 'Invalid token!')
 
